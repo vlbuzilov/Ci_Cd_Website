1bc0534bca9d269ccc595040705646bae6af2ba2
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-05-23 21:29:09 +0300

    -

--- djangoproject/shop_webapp/models.py
+++ djangoproject/shop_webapp/models.py
@@ -13,8 +13,9 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='static/images')
     isDiscount = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
 
@@ -23,6 +24,7 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
 
@@ -35,4 +37,3 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
-
